99b2a871e8d5cb47c33d4cc259ca2fa49b6ff411
khizer <mekhizer@gmail.com>
2026-06-29 21:20:05 +0100

    Created function to accept incoming connections

--- utilities/socketutil.c
+++ utilities/socketutil.c
@@ -22,3 +22,16 @@ struct sockaddr_in *create_ipv4_address(char *ip, int port)
 
     return address;
 }
+
+struct accepted_socket *accept_connection(int server_socket_fd)
+{
+    struct accepted_socket *accepted_socket = malloc(sizeof(struct accepted_socket));
+    accepted_socket->address = NULL;
+    socklen_t address_size = sizeof(struct sockaddr_in);
+
+    // accept incoming connection and populate struct
+    accepted_socket->socket_fd = accept(server_socket_fd, (struct sockaddr *) accepted_socket->address, &address_size);
+    accepted_socket->accepted = accepted_socket->socket_fd > 0;
+
+    return accepted_socket;
+}
--- utilities/socketutil.h
+++ utilities/socketutil.h
@@ -3,16 +3,28 @@
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 
+struct accepted_socket
+{
+    int socket_fd;
+    struct sockaddr_in *address;
+    bool accepted;
+};
+
 /* Create and return a file descriptor for a TCP IPv4 socket. */
 int create_tcp_ipv4_socket();
 
-/* Create SOCKADDR_IN struct, initialise it with IP and PORT
- * and return a pointer to it. */
+/* Create SOCKADDR_IN struct, initialise it with IP and PORT and return
+ * a pointer to it. */
 struct sockaddr_in *create_ipv4_address(char *ip, int port);
+
+/* Accept incoming connection, create ACCEPTED_SOCKET struct and return
+ * a pointer to it. */
+struct accepted_socket *accept_connection(int server_socket_fd);
 
 #endif
