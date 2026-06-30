ef5a066508dfcedcfe03edf4b683449bce9cef51
khizer <mekhizer@gmail.com>
2026-07-01 00:32:37 +0100

    Added concurrency to handle multiple clients at once

--- server/server.c
+++ server/server.c
@@ -1,79 +1,100 @@
 #include "../utilities/socketutil.h"
-#include <err.h>
+#include <errno.h>
+#include <error.h>
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-struct accepted_socket *accept_connection(int server_socket_fd);
-int recv_and_write_msg(int sock_fd);
+#define MAX_CONNECTIONS 5
+
+int recv_and_write_msg(struct accepted_socket *accepted_socket);
 
 int main()
 {
-    int server_socket_fd;
-    struct sockaddr_in *server_address;
-    struct accepted_socket *client_socket;
+    int socket_fd;
+    struct sockaddr_in *address = NULL;
+    struct accepted_socket *accepted_socket;
 
-    // create server socket
-    server_address = create_ipv4_address("", 49153);
-    server_socket_fd = create_tcp_ipv4_socket();
-    if (server_socket_fd == -1)
-        errx(EXIT_FAILURE, "Error encountered when creating TCP IPv4 socket");
+    // create the server's socket fd and address
+    create_ipv4_address(&address, "", 8080);
+    if ((socket_fd = create_tcp_ipv4_socket()) == -1)
+        error(EXIT_FAILURE, errno, "creating socket failed");
 
-    // bind server socket to server address
-    int bind_flag = bind(server_socket_fd, (struct sockaddr *) server_address, sizeof(*server_address));
-    if (bind_flag == -1)
-        errx(EXIT_FAILURE, "Server socket could not be bound");
+    // bind the server's socket fd and address
+    if (bind(socket_fd, (struct sockaddr *) address, sizeof(struct sockaddr)) == -1)
+        error(EXIT_FAILURE, errno, "bind failed");
 
     printf("Server socket was bound successfully\n");
 
-    // accept any incoming requests
-    if ((listen(server_socket_fd, 5)) == -1)
-        errx(EXIT_FAILURE, "Error encountered when listening for connections");
+    // listen for incoming connection requests, queue upto MAX_CONNECTIONS connections
+    if ((listen(socket_fd, MAX_CONNECTIONS)) == -1)
+        error(EXIT_FAILURE, errno, "listen failed");
 
-    client_socket = accept_connection(server_socket_fd);
-    if (!client_socket->accepted)
-        errx(EXIT_FAILURE, "Error accepting incoming connection");
-
-    // create child process to handle accepted client
-    if (!fork())
+    // accept connections and create a child process to handle each connection
+    int n_children;
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
     {
-        // receive message from the client and print to stdout
-        char buffer[1024];
-        while (true)
+        accepted_socket = accept_connection(socket_fd);
+
+        if (accepted_socket->accepted)
         {
-            int n_recv = recv_and_write_msg(client_socket->socket_fd);
+            if (!fork())
+            {
+                n_children++;
 
-            if (n_recv == -1)
-                errx(EXIT_FAILURE, "Error receiving requests/messages");
+                // receive messages from connected peer and write them to stdout
+                while (true)
+                {
+                    int n_recv = recv_and_write_msg(accepted_socket);
 
-            if (n_recv == 0)
-                break;
+                    if (n_recv == -1)
+                        error(0, errno, "receive failed");
+                    else if (n_recv == 0)
+                        break;
+                }
+
+                close(accepted_socket->socket_fd);
+                free(accepted_socket);
+            }
         }
+        else
+            error(0, errno, "accept failed");
     }
-    else
+
+    // parent waits for all children
+    for (int i = 0; i < n_children; i++)
         wait(NULL);
 
     // close files and free memory
-    close(client_socket->socket_fd);
-    close(server_socket_fd);
-    free(server_address);
-    free(client_socket);
+    close(socket_fd);
+    free(address);
+    if (accepted_socket)
+        free(accepted_socket);
+
     return 0;
 }
 
 /* Receive message from SOCK_FD and print it to stdout. Return number
  * of bytes received or -1 for error. */
-int recv_and_write_msg(int sock_fd)
+int recv_and_write_msg(struct accepted_socket *accepted_socket)
 {
     char buffer[1024];
-    int n_recv = recv(sock_fd, buffer, sizeof(buffer), 0);
+    int n_recv = recv(accepted_socket->socket_fd, buffer, sizeof(buffer), 0);
 
     if (n_recv > 0)
     {
+        char ip[INET_ADDRSTRLEN];
+        short port;
+
+        // converting ip and port to human readable format
+        inet_ntop(accepted_socket->address.sin_family, &accepted_socket->address.sin_addr.s_addr, ip, sizeof(ip));
+        port = ntohs(accepted_socket->address.sin_port);
+
         buffer[n_recv] = 0;
-        printf("Response was: %s\n", buffer);
+        printf("Message from %s:%hu = %s \n", ip, port, buffer);
     }
 
     return n_recv;
