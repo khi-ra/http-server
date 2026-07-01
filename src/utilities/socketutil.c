2186db1eeec37dd496db7788192edfb1e540266e
khizer <mekhizer@gmail.com>
2026-07-01 23:06:20 +0100

    Refactored polling logic to a separate method, #2

--- src/utilities/socketutil.c
+++ src/utilities/socketutil.c
@@ -1,6 +1,8 @@
 #include "socketutil.h"
 #include <err.h>
 #include <netinet/in.h>
+#include <poll.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 int create_tcp_ipv4_socket()
@@ -23,19 +25,40 @@ void create_ipv4_address(struct sockaddr_in **addr, char *ip, int port)
         inet_pton(AF_INET, ip, &(*addr)->sin_addr.s_addr);
 }
 
-struct accepted_socket *accept_connection(int socket_fd)
+struct accepted_socket *accept_connection(int socket_fd, int timeout_ms)
 {
+    // create and zero-fill accepted socket struct
+    struct accepted_socket *accepted_socket = malloc(sizeof(struct accepted_socket));
+    memset(accepted_socket, 0, sizeof(struct accepted_socket));
+
+    // poll for read event
+    if (poll_read_event(socket_fd, timeout_ms) <= 0)
+    {
+        accepted_socket->accepted = false;
+        return accepted_socket;
+    }
+
     // accept connection
     struct sockaddr_in addr;
     socklen_t addr_size = sizeof(struct sockaddr_in);
     int fd = accept(socket_fd, (struct sockaddr *) &addr, &addr_size);
 
-    // create and populate struct
-    struct accepted_socket *accepted_socket = malloc(sizeof(struct accepted_socket));
-
+    // populate struct
     accepted_socket->socket_fd = fd;
     accepted_socket->address = addr;
     accepted_socket->accepted = accepted_socket->socket_fd > 0;
 
     return accepted_socket;
+}
+
+int poll_read_event(int socket_fd, int duration_ms)
+{
+    // initialise poll file descriptor struct for read events
+    struct pollfd pfd;
+    pfd.fd = socket_fd;
+    pfd.events = POLLIN;
+
+    // wait for duration for read events
+    int poll_fds = poll(&pfd, 1, duration_ms);
+    return poll_fds;
 }
