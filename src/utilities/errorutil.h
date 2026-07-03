3d6524dc430c5fcdd2789dc0935f0e1fff06a7c0
Khizer Rafiq <mekhizer@gmail.com>
2026-07-03 22:43:03 +0100

    Changed include of `errorutil.h` and `errorutil.c` (#7)

--- src/utilities/errorutil.h
+++ src/utilities/errorutil.h
@@ -1,4 +1,12 @@
+#ifndef ERRORUTIL_H
+#define ERRORUTIL_H
+
+#include <stdio.h>
+#include <string.h>
+
 /* Print a message with `fprintf (stderr, FORMAT, ...)`.
- * If ERRNUM is nonzero, follow it with ": " and strerror (ERRNUM).
- * If STATUS is nonzero, terminate the program with `exit (STATUS)`.*/
-void error_handler(int errnum, char* msg);
+ * If ERRNUM is nonzero, follow it with ": " and `strerror(ERRNUM)`.
+ * If STATUS is nonzero, terminate the program with `exit(STATUS)`.*/
+void error_handler(int errnum, char *msg);
+
+#endif
