7d4942ff961419d4e50e50ecd93f3604334c5a29
Khizer Rafiq <mekhizer@gmail.com>
2026-07-04 01:35:17 +0100

    Removed use `strcat()` due to trace trap error (#7)

--- src/utilities/errorutil.c
+++ src/utilities/errorutil.c
@@ -3,7 +3,7 @@
 void error_handler(int errnum, char *msg)
 {
     if (msg)
-        fprintf(stderr, "%s%s\n", msg, errnum == 0 ? "" : strcat(": ", strerror(errnum)));
+        fprintf(stderr, "%s: %s\n", msg, errnum == 0 ? "" : strerror(errnum));
     else if (errnum != 0)
         fprintf(stderr, "%s\n", strerror(errnum));
 }
