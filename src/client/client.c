e85bdb465261b1d6d57938cd4a9205a10be8b7ed
Khizer Rafiq <mekhizer@gmail.com>
2026-07-04 01:37:34 +0100

    Changed logic in `client.c` to accomodate `error_handler()`

--- src/client/client.c
+++ src/client/client.c
@@ -1,6 +1,6 @@
+#include "../utilities/errorutil.h"
 #include "../utilities/socketutil.h"
 #include <errno.h>
-#include <error.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -8,38 +8,42 @@ int read_input(char **line);
 
 int main()
 {
-    struct sockaddr_in *server_address = NULL;
+    struct sockaddr_in server_address;
+    int setup_successful = 1;
 
     int socket_fd = create_tcp_ipv4_socket();
-    create_ipv4_address(&server_address, "127.0.0.1", 8080);
+    server_address = create_ipv4_address("127.0.0.1", 8080);
 
     // send connection request to server
-    if (connect(socket_fd, (struct sockaddr *) server_address, sizeof(*server_address)) == -1)
-        error(EXIT_FAILURE, errno, "connect failed");
+    if (connect(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address)) == -1)
+    {
+        error_handler(errno, "connect failed");
+        setup_successful = 0;
+    }
 
-    printf("Connection was successful\n");
+    if (setup_successful)
+        printf("Connection was successful\n");
 
     // read input from stdin and send to server
     char *line = NULL;
     printf("Enter input to send to the server ('exit' to stop):\n");
 
-    while (true)
+    while (setup_successful)
     {
         int n_read = read_input(&line);
 
         if (n_read == -1)
-            error(EXIT_FAILURE, errno, "reading user input failed:");
+            error_handler(errno, "reading user input failed:");
 
         if (strcmp(line, "exit\n") == 0)
             break;
 
         if (send(socket_fd, line, n_read, 0) == -1)
-            error(EXIT_FAILURE, errno, "sending message to server failed");
+            error_handler(errno, "sending message to server failed");
     }
 
-    // close file and free memory
+    // cleanup
     close(socket_fd);
-    free(server_address);
     free(line);
     return 0;
 }
