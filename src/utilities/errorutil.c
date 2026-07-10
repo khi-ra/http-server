c3b843abf378e5e192c445a36ae9a38eae80c9b3
khizer <mekhizer@gmail.com>
2026-07-10 15:29:31 +0100

    Added enum for error codes and a lookup table to map each error code to a string. (#15)

--- src/utilities/errorutil.c
+++ src/utilities/errorutil.c
@@ -1,9 +1,24 @@
 #include "errorutil.h"
 
-void error_handler(int errnum, char *msg)
+static const char *error_messages[] = {
+    [ERR_SETUP] = "Server setup failed",
+    [ERR_ACCEPT] = "Accepting connection failed",
+    [ERR_SOCK_IO] = "Socket I/O failed",
+    [ERR_SOCK_TIMEOUT] = "Socket idle timeout",
+    [ERR_EVENTFD] = "Events file I/O failed",
+    [ERR_POLL] = "Polling for connection failed",
+    [ERR_THREAD] = "Creating/Detaching thread failed",
+};
+
+void error_handler(int errnum, const char *msg)
 {
     if (msg)
         fprintf(stderr, "%s: %s\n", msg, errnum == 0 ? "" : strerror(errnum));
     else if (errnum != 0)
         fprintf(stderr, "%s\n", strerror(errnum));
+}
+
+const char *err_str(int errnum)
+{
+    return error_messages[errnum];
 }
--- src/utilities/errorutil.h
+++ src/utilities/errorutil.h
@@ -4,9 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 
+enum error_codes
+{
+    ERR_SETUP,        /* Creating socket fd, binding, or listening failed. */
+    ERR_ACCEPT,       /* Accepting the incoming connection failed. */
+    ERR_SOCK_IO,      /* Sending or receiving message over the connection failed. */
+    ERR_SOCK_TIMEOUT, /* Socket timed out, client was inactive for 60s. */
+    ERR_EVENTFD,      /* Writing to or reading from the events file failed.  */
+    ERR_POLL,         /* Polling failed. */
+    ERR_THREAD,       /* Creating or detaching thread failed. */
+};
+
 /* Print MSG and ERRNUM's associated error message to stderr.
  * If MSG is a null pointer, print only ERRNUM's error message.
  * If ERRNUM is zero, print MSG followed by a colon and a space. */
-void error_handler(int errnum, char *msg);
+void error_handler(int errnum, const char *err_msg);
 
 #endif
