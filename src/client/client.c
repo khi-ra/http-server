ba1a7a4841c917f787b9396ce454802dc25867c9
khizer <mekhizer@gmail.com>
2026-07-10 19:17:37 +0100

    Updated client.c to use goto and custom error codes. (#15)

--- src/client/client.c
+++ src/client/client.c
@@ -1,56 +1,75 @@
 #include "../utilities/errorutil.h"
 #include "../utilities/socketutil.h"
+#include <asm-generic/errno.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int read_input(char **line);
+static int errnum;
+
+static int setup(int *fd, struct sockaddr_in *server_addr, char *ip, int port);
+static int read_input(char **line);
+
+int setup(int *fd, struct sockaddr_in *server_addr, char *ip, int port)
+{
+    int socket_fd;
+    int err_flag = -1;
+
+    *server_addr = create_ipv4_address(ip, port);
+    socket_fd = create_tcp_ipv4_socket();
+    if (socket_fd == -1)
+        goto out;
+
+    int connect_result = connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(*server_addr));
+    if (connect_result == -1)
+        goto out;
+
+    *fd = socket_fd;
+    err_flag = 0;
+
+out:
+    if (err_flag == -1)
+        errnum = ERR_SETUP;
+
+    return err_flag;
+}
 
 int main()
 {
-    struct sockaddr_in server_address;
     int socket_fd;
+    struct sockaddr_in server_address;
     char *line = NULL;
-    int setup_successful = 1;
 
-    server_address = create_ipv4_address("127.0.0.1", 8080);
-    if ((socket_fd = create_tcp_ipv4_socket()) == -1)
-    {
-        error_handler(errno, "server socket creation failed");
-        setup_successful = 0;
-    }
-    else if (connect(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address)) == -1)
-    {
-        error_handler(errno, "connect failed");
-        setup_successful = 0;
-    }
+    int setup_result = setup(&socket_fd, &server_address, "127.0.0.1", 8080);
+    if (setup_result == -1)
+        goto error_out;
 
-    if (setup_successful)
-    {
-        printf("Connection was successful\n");
-        printf("Enter input to send to the server ('exit' to stop):\n");
-    }
+    printf("Connection was successful\n");
+    printf("Enter input to send to the server ('exit' to stop):\n");
 
     // read input from stdin and send to server
-    while (setup_successful)
+    while (true)
     {
         int n_read = read_input(&line);
 
         if (n_read == -1)
-            error_handler(errno, "reading user input failed:");
-
-        if (strcmp(line, "exit\n") == 0)
             break;
 
-        if (send(socket_fd, line, n_read, 0) == -1)
-            error_handler(errno, "sending message to server failed");
+        if (strcmp(line, "exit\n") == 0)
+            goto out;
+
+        int n_sent = send(socket_fd, line, n_read, 0);
+        if (n_sent == -1 && errnum == ERR_SOCK_IO)
+            break;
     }
 
-    // cleanup
+    error_handler(errno, err_str(errnum));
+
+out:
     close(socket_fd);
     if (line)
         free(line);
-    return 0;
 }
 
 /* Read input line-by-line from stdin and store it in LINE.
@@ -58,10 +77,9 @@ int main()
 int read_input(char **line)
 {
     size_t line_size = 0;
-    ssize_t n_read = getline(line, &line_size, stdin);
-
+    int n_read = getline(line, &line_size, stdin);
     if (n_read == -1 && (errno == EINVAL || errno == ENOMEM))
-        return -1;
+        errnum = ERR_SOCK_IO;
 
     return n_read;
 }
